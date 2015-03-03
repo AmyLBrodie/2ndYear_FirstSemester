cc59af6ad67ff51b2d69be835d060835374bb3e2
Amy Brodie <brdamy004@myuct.ac.za>
2015-03-03 22:21:38 +0200

    Prac1: Implementation for the similar, levelZeroNodeList and levelNplusOneNodeList methods. The similar method checks if the structure of each BST is similar through recursion, returns true if similar and false if not similar. The levelZeroNodeList method creates a string with the node in level zero (if there is one) and returns it. The levelNplusOneNodeList method creates a string with all the nodes in a specific level of the BST (also accounts for empty nodes) and returns the string.

--- Trees_1/JavaComponents/TreeUtils.java
+++ Trees_1/JavaComponents/TreeUtils.java
@@ -6,16 +6,80 @@
  * @author Amy Brodie
  * @version 01/03/2015
  */
+
+import java.util.*;
+
 public class TreeUtils {
-    public boolean similar(){
+    
+    private int level = 0, height1, height2;
+    
+    public boolean similar(BinaryTreeNode node1, BinaryTreeNode node2){
+        if (level == 0){
+            height1 = node1.getHeight();
+            height2 = node2.getHeight();
+        }
         
+        if (height1 == height2){
+            if (level == 0 && levelZeroNodeList(node1).equals("") && levelZeroNodeList(node2).equals("")){
+                return true;
+            }
+            else if (level == 0 && ((!levelZeroNodeList(node1).equals("") && levelZeroNodeList(node2).equals("")) || (levelZeroNodeList(node1).equals("") && !levelZeroNodeList(node2).equals("")))){
+               return false;   
+            }
+            else{
+                level += 1;
+                if (node1.hasLeft() && node2.hasLeft()){
+                    if (height1-1 == level && (!node1.hasRight() && !node2.hasRight()) || (node1.hasRight() && node2.hasRight())){
+                       return true;    
+                    }
+                    else if (height1-1 != level){
+                       return similar(node1.getLeft(), node2.getLeft());
+                    }
+                    else{
+                       return false; 
+                    }
+                }
+                else if (node1.hasRight() && node2.hasRight()) {
+                    if (height1-1 == level && (!node1.hasLeft() && !node2.hasLeft()) || (node1.hasLeft() && node2.hasLeft())){
+                       return true;    
+                    }
+                    else if (height1-1 != level){
+                       return similar(node1.getRight(), node2.getRight());
+                    }
+                    else{
+                       return false; 
+                    }    
+                }
+            }
+        }
+        else{
+            return false;
+        }
     }
     
-    public String levelZeroNodeList(){
-        
+    public String levelZeroNodeList(BinaryTreeNode temp){
+        if (temp.getHeight() == 0){
+            return "";
+        }
+        else{
+            return Integer.toString(temp.getItem());
+        }
+           
     }
     
-    public String levelNplusOneNodeList(){
+    public String levelNplusOneNodeList(BinaryTreeNode temp){
+        if (temp.hasLeft() && temp.hasRight()){
+            return temp.getLeft() + "#" + temp.getRight() + "#";
+        }
+        else if (temp.hasLeft()){
+            return temp.getLeft() + "#null#";
+        }
+        else if (temp.hasRight()){
+            return "null#" + temp.getRight() + "#";
+        }
+        else{
+            return "";
+        }
         
     }
     
