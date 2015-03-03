0183638c34e9f54b318bed9c787585565a300a73
Amy Brodie <brdamy004@myuct.ac.za>
2015-03-03 18:55:07 +0200

    Prac1: The driver class for the binary search tree was implemented. The input is asked for and then sorted into the BST.

--- Trees_1/JavaComponents/BST_main.java
+++ Trees_1/JavaComponents/BST_main.java
@@ -18,13 +18,33 @@ public class BST_main {
         SimpleBST searchTreeOne = new SimpleBST(), searchTreeTwo = new SimpleBST();
         int tempOne = 0, tempTwo = 0;
         
-        while (treeOne != null){
-            tempOne = Integer.parseInt(treeOne.substring(0,treeOne.indexOf(",")));
-            treeOne = treeOne.substring(treeOne.indexOf(",")+2);
-            System.out.println(treeOne);
+        while (!treeOne.equals("")){
+         
+            if (treeOne.indexOf(",") == -1){
+                tempOne = Integer.parseInt(treeOne);
+                treeOne = "";
+            }
+            else{
+                tempOne = Integer.parseInt(treeOne.substring(0,treeOne.indexOf(",")));
+                treeOne = treeOne.substring(treeOne.indexOf(",")+2);
+            }
             
+            searchTreeOne.insert(tempOne);
         }
         
+        while (!treeTwo.equals("")){
+        
+            if (treeTwo.indexOf(",") == -1){
+                tempTwo = Integer.parseInt(treeTwo);
+                treeTwo = "";
+            }
+            else{
+                tempTwo = Integer.parseInt(treeTwo.substring(0,treeTwo.indexOf(",")));
+                treeTwo = treeTwo.substring(treeTwo.indexOf(",")+2);
+            }
+            
+            searchTreeTwo.insert(tempTwo);
+        }
         
         
     }
